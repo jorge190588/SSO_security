2ff9168c93eae7a7fb8915e25e1041a744bea5d3
jorge190588 <netneill@hotmail.com>
2019-09-17 21:54:17 -0600

    reorganize imports in menu.js

--- react-social/src/components/Menu.js
+++ react-social/src/components/Menu.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import {
-  Route,
-  Switch,
-  Link
-} from 'react-router-dom';
+import { Route,  Switch, Link } from 'react-router-dom';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-
-import { Button, Divider } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
+import {ListItem, ListItemIcon, ListItemText, IconButton, Drawer, CssBaseline, AppBar , 
+        Toolbar, List, Typography, Button, Divider } from '@material-ui/core';
+ 
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 

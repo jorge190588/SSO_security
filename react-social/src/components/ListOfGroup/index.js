c3ecee16951f5fbca1a2ea06b091d4952e2a2247
Jorge Santos <netneill@hotmail.com>
2019-09-19 10:56:55 -0600

    split style of ListOfGroup component

--- react-social/src/components/ListOfGroup/index.js
+++ react-social/src/components/ListOfGroup/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {ListItem, ListItemIcon, ListItemText, List //, ListSubheader
-        } from '@material-ui/core';
+import {ListItem, ListItemIcon, ListItemText, List } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Collapse from '@material-ui/core/Collapse';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-    nested: {
-      paddingLeft: theme.spacing(4),
-    },
-  }));
+import {useStyles} from './style';
 
 export default function ListOfGroup(props) {
-
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
 
@@ -32,42 +19,29 @@ export default function ListOfGroup(props) {
     }
 
     return (
-       
-      <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      /*subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          {props.listOfGroup[0].name}
-        </ListSubheader>
-      }*/
-      className={classes.root}>
-      
-     
-      
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary={props.listOfGroup[0].name} />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-            {props.listOfGroup.map((option, index) => (
-                (index>0) ? (
-                    <ListItem button key={option.name}  to={option.url} className={classes.nested} >
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <InboxIcon />}</ListItemIcon>
-                        <ListItemText primary={option.name} />
-                    </ListItem>
-                ) :null 
-                
-            ))}
+        <List component="nav" aria-labelledby="nested-list-subheader" className={classes.root}>
+            <ListItem button onClick={handleClick}>
+                <ListItemIcon>
+                <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={props.listOfGroup[0].name} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+        
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    {props.listOfGroup.map((option, index) => (
+                        (index>0) ? (
+                            <ListItem button key={option.name}  to={option.url} className={classes.nested} >
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <InboxIcon />}</ListItemIcon>
+                                <ListItemText primary={option.name} />
+                            </ListItem>
+                        ) :null 
+                        
+                    ))}
+                </List>
+            </Collapse>
         </List>
-      </Collapse>
-
-    </List>
    );
   
 }

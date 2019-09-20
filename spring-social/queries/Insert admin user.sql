dc4ae4f7cd006ed5a079489bc98ae34ba4ad5fbb
Jorge Santos <netneill@hotmail.com>
2019-09-20 11:37:07 -0600

    add user page in sql sentences

--- spring-social/queries/Insert admin user.sql
+++ spring-social/queries/Insert admin user.sql
@@ -31,7 +31,8 @@ values
 ('Inicio',1,'/',1,'home'),
 ('Barriles',1,'/barril',1,'delete_outline'),
 ('Tarimas',1,'/tarima',1,'dns'),
-('Mi perfil',2,'/profile',1,'person');
+('Mi perfil',2,'/profile',1,'person'),
+('Usuarios',2,'/user',1,'people');
 
 insert into action (name,path,method_id) 
 values ('Crear','create',1),('Modificar','update',2),('Anular','cancel',5),('Listado','List',1),('Ver','view',1);
@@ -41,14 +42,16 @@ values
 (1,1,1),(1,2,2),(1,3,3),(1,4,4),(1,5,5),
 (2,1,1),(2,2,2),(2,3,3),(2,4,4),(2,5,5),
 (3,1,1),(3,2,2),(3,3,3),(3,4,4),(3,5,5),
-(4,1,1),(4,2,2),(4,3,3),(4,4,4),(4,5,5);
+(4,1,1),(4,2,2),(4,3,3),(4,4,4),(4,5,5),
+(5,1,1),(5,2,2),(5,3,3),(5,4,4),(5,5,5);
 
 insert into rol_form_action (rol_id,form_action_id) 
 values 
 (1,1),(1,2),(1,3),(1,4),(1,5),
 (1,6),(1,7),(1,8),(1,9),(1,10),
 (1,11),(1,12),(1,13),(1,14),(1,15),
-(1,16),(1,17),(1,18),(1,19),(1,20);
+(1,16),(1,17),(1,18),(1,19),(1,20),
+(1,21),(1,22),(1,23),(1,24),(1,25);
 
 
 insert into users (email,email_verified, image_url,name,password,provider,provider_id,rol_id) 

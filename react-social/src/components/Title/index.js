173df9ef4ea83fb95813cc6f3e7722a220c230d8
jorge190588 <netneill@hotmail.com>
2019-09-21 12:00:51 -0600

    update title component, receive the icon and title parameters

--- react-social/src/components/Title/index.js
+++ react-social/src/components/Title/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import Icon from '@material-ui/core/Icon';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,17 +15,22 @@ const useStyles = makeStyles(theme => ({
   },
   h1:{
       color: '#000'
+  },
+  icon:{
+    "padding-top": 5
   }
 }));
 
 export default function Title(props) {
   const classes = useStyles();
-
+//<Paper className={classes.paper}></Paper>
   return (
       <div>
-          <Paper className={classes.paper}>
-            <h1 className={classes.h1}>{props.title}</h1>
-          </Paper>
+          
+          <h1 className={classes.h1}>
+            <Icon className={classes.icon} fontSize="large" >{(props.icon) ? props.icon : "check"}</Icon> {props.title}
+            </h1>
+          <Divider />
       </div>
   );
 }

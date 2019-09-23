d88f445c1039bfb2c7323187aede136c5215e2a3
Jorge Santos <netneill@hotmail.com>
2019-09-23 08:44:35 -0600

    refactor title component in front end project

--- react-social/src/components/Title/index.js
+++ react-social/src/components/Title/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+
 import Divider from '@material-ui/core/Divider';
 import Icon from '@material-ui/core/Icon';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'left',
-    color: theme.palette.text.secondary,
   },
   h1:{
       color: '#000'
@@ -23,7 +18,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function Title(props) {
   const classes = useStyles();
-//<Paper className={classes.paper}></Paper>
+
   return (
       <div>
           

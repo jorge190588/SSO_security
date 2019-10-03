780a9afa73cef25d050cfcd9141bad34cb5b53cb
Jorge Santos <netneill@hotmail.com>
2019-10-03 09:07:37 -0600

    add add, update, cancel and view event to table component

--- react-social/src/components/Table/index.js
+++ react-social/src/components/Table/index.js
@@ -3,83 +3,95 @@ import MaterialTable from 'material-table';
 import TablePagination from '@material-ui/core/TablePagination';
 
 export default function Table(props) {
-  const [state] = React.useState({
-    columns: props.header,
-    data: props.data,
-    rowsPerPage: 10
-  });
+    const actionsList =[];
 
-   
-  return (
-    <div>
-        <MaterialTable minRows={10}
-        
-        localization={{
-        toolbar: {
-            searchPlaceholder: "Buscar",
-            searchTooltip: "Buscar "
-        },
-        pagination:{
-            labelRowsSelect:"Registros",
-            labelRowsPerPage:"Filas por pagina"
-        },
-        body: {
-            deleteTooltip: "Eliminar",
-            emptyDataSourceMessage: "No existen registros"
-        }
-        }}
-        title="Listado de registros"
-        columns={state.columns}
-        data={state.data}
-        actions={[
-            {
+    if (props.addRegister){
+        actionsList.push({
             icon: 'add',
             tooltip: 'Agregar',
             isFreeAction: true,
             onClick: props.addRegister
-            },
-            {
-                icon: 'save',
-                tooltip: 'Modificar',
-                onClick:  props.updateRegister//(event, rowData) => alert("Modificar " + rowData.name)
-              },
-              rowData => ({
-                icon: 'delete',
-                tooltip: 'Eliminar',
-                onClick: (event, rowData) => alert("Eliminar ? " + rowData.name),
-                //disabled: rowData.birthYear < 2000
-              }),
-              rowData => ({
-                  icon: 'visibility',
-                  tooltip: 'Ver información',
-                  onClick: props.updateRegister// (event, rowData) => alert("Ver informacion ? " + rowData.name),
-                  //disabled: rowData.birthYear < 2000
-                }),
-        ]}
+        });
+    }
 
-        options={{
-            pageSize: 10,
-            pageSizeOptions: [5, 10, 20, 30 ,50, 75, 100 ],
-            toolbar: true,
-            paging: true,
-            actionsColumnIndex: -1
-        }}
-         
-        components={{
-             
-            Pagination: props => (
-              <TablePagination
-                {...props}
-                labelRowsPerPage={<div style={{fontSize: 14}}>{props.labelRowsPerPage}</div>}
-                labelDisplayedRows={row => <div style={{fontSize: 14}}>{props.labelDisplayedRows(row)}</div>}
-                SelectProps={{
-                  style:{
-                    fontSize: 14
-                  }
-                }}                    
-              />
-            )
-          }}
+    if (props.updateRegister){
+        actionsList.push({icon: 'save',
+        tooltip: 'Modificar',
+        onClick:  props.updateRegister});
+    }
+
+    if (props.viewRegister){
+        actionsList.push(function(rowData){ 
+            return {
+                icon: 'visibility',
+                tooltip: 'Ver información',
+                onClick: props.viewRegister
+            }
+        });
+    }
+
+    if (props.cancelRegister){
+        actionsList.push(function(rowData){ 
+            return {
+                icon: 'delete',
+                tooltip: 'Cancelar registro',
+                onClick: props.cancelRegister
+            }
+        });
+    }
+
+    const [state] = React.useState({
+        columns: props.header,
+        data: props.data,
+        rowsPerPage: 10
+    });
+
+   
+  return (
+    <div>
+        <MaterialTable
+            localization={{
+            toolbar: {
+                searchPlaceholder: "Buscar",
+                searchTooltip: "Buscar "
+            },
+            pagination:{
+                labelRowsSelect:"Registros",
+                labelRowsPerPage:"Filas por pagina"
+            },
+            body: {
+                deleteTooltip: "Eliminar",
+                emptyDataSourceMessage: "No existen registros"
+            }
+            }}
+            title="Listado de registros"
+            columns={state.columns}
+            data={state.data}
+            actions={actionsList}
+
+            options={{
+                pageSize: 10,
+                pageSizeOptions: [5, 10, 20, 30 ,50, 75, 100 ],
+                toolbar: true,
+                paging: true,
+                actionsColumnIndex: -1
+            }}
+            
+            components={{
+                
+                Pagination: props => (
+                <TablePagination
+                    {...props}
+                    labelRowsPerPage={<div style={{fontSize: 14}}>{props.labelRowsPerPage}</div>}
+                    labelDisplayedRows={row => <div style={{fontSize: 14}}>{props.labelDisplayedRows(row)}</div>}
+                    SelectProps={{
+                    style:{
+                        fontSize: 14
+                    }
+                    }}                    
+                />
+                )
+            }}
         >
         
         </MaterialTable>

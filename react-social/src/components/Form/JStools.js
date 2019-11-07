df3794df8378ca9abaa544bca6ac87cd548a0c14
Jorge Santos <netneill@hotmail.com>
2019-11-07 15:16:45 -0600

    add JStools for forms

---
+++ react-social/src/components/Form/JStools.js
@@ -0,0 +1,23 @@
+const setValuesToElements = (elements, data)=>{
+    Object.keys(data).forEach(key =>   {
+        if(elements[key]!==undefined)   elements[key].value=data[key] ;
+    })
+    return elements;
+}
+
+const cleanValuesToElements= (elements)=>{
+    Object.keys(elements).map(key => elements[key].value='');
+    return elements;
+}
+
+const setErrorsToElements = (newData,elements)=>{   
+    newData.error.messageList.forEach(function(entry) {
+        elements[entry.attribute].errorMessages=entry.message;
+        elements[entry.attribute].isError=true;
+    });
+    return elements;
+}
+
+const JSTools = {setValuesToElements, cleanValuesToElements,setErrorsToElements};
+
+export default JSTools;

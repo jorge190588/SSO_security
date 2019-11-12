19b2f4c3619b7bf58d8ee7171eee48d6aea1247e
Jorge Santos <netneill@hotmail.com>
2019-11-12 10:13:58 -0600

    comment unused code in login

--- tecun-security-front/src/pages/Security/Login/index.js
+++ tecun-security-front/src/pages/Security/Login/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './style.css';
-import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL, ACCESS_TOKEN } from '../../../constants';
+import {/*  GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL, */ACCESS_TOKEN } from '../../../constants';
 import { login } from 'services/User';
 import { /*Link,*/ Redirect } from 'react-router-dom'
-import fbLogo from '../../../img/fb-logo.png';
+/*import fbLogo from '../../../img/fb-logo.png';
 import googleLogo from '../../../img/google-logo.png';
 import githubLogo from '../../../img/github-logo.png';
+*/
 import Alert from 'react-s-alert';
 
 class Login extends Component {
